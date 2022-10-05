80d3543f22c871554e419156f394bcdb5cc77026
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-10-05 13:19:58 +0000

    addresses_column

---
+++ db/migrate/20221005131602_add_columns_to_addresses.rb
@@ -0,0 +1,8 @@
+class AddColumnsToAddresses < ActiveRecord::Migration[6.1]
+  def change
+    add_column :addresses, :customer_id, :integer, null: false
+    add_column :addresses, :name, :string, null: false
+    add_column :addresses, :postal_code, :string, null: false
+    add_column :addresses, :address, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_04_134545) do
+ActiveRecord::Schema.define(version: 2022_10_05_131602) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
   end
 
   create_table "admins", force: :cascade do |t|
