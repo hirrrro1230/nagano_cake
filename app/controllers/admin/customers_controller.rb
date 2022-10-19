6ccb899c2375ae6317cd7ef54e5c35a28a997039
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-10-19 08:06:48 +0000

    10191706

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,10 @@
 class Admin::CustomersController < ApplicationController
     before_action :if_not_admin
     
+    def index
+        @customers = Customer.all
+    end
+    
     private
     def if_not_admin
     redirect_to root_path unless current_customer.admin?
