8431004e424598c20083fbb573d0c6a487d7981b
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-10-23 08:05:50 +0000

    10231705

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,7 @@
 class Admin::HomesController < ApplicationController
+    def top
+        @orders = Order.all
+        @order = Order.find(params[:id])
+        @customer = Customer.find(@order.id)
+    end
 end
