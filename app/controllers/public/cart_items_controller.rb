25ae40a826f494f4c458202312b3e8da96d422ea
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-10-24 13:45:44 +0000

    10242245

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,6 +14,18 @@ class Public::CartItemsController < ApplicationController
             redirect_to cart_items_path
         end
     end
+    
+    def destroy
+        @cart_item = CartItem.find(params[:id])
+        @cart_item.destroy!
+        redirect_to cart_items_path
+    end
+    
+    def destroy_all
+        cart_items = CartItem.all
+        cart_items.destroy_all!
+        redirect_to cart_items_path
+    end
 
     private
       def cart_item_params
