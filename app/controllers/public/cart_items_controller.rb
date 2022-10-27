f6878abb7d1ed63faf5bfda996015fd3e323e781
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-10-27 12:35:31 +0000

    cart_itemのindex編集10/27

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -15,9 +15,22 @@ class Public::CartItemsController < ApplicationController
     def create
         @cart_item = CartItem.new(cart_item_params)
         @cart_item.customer_id = current_customer.id
-        if @cart_item.save!
-            redirect_to cart_items_path
+        @cart_items = current_customer.cart_items
+        @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+            new_amount = cart_item.amount + @cart_item.amount
+            cart_item.update(amount: new_amount)
+            return redirect_to cart_items_path
+        end 
         end
+        @cart_item.save
+        redirect_to :cart_items
+    end
+    
+    def update
+        cart_item = CartItem.find(params[:id])
+        cart_item.update(cart_item_params)
+        redirect_to cart_items_path
     end
     
     def destroy
