8ad6fc667f4b5ac0f95ac54af59b2ddecf03d454
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-10-29 14:05:22 +0000

    10292305

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,7 +13,20 @@ class Public::CustomersController < ApplicationController
         @customer = Customer.find(params[:id])
     end
     
+    def update
+        @customer = Customer.find(params[:id])
+        if customer.update(cusotmer_params)
+          redirect_to customer_path(@customer.id), notice: "You have updated user successfully."
+        else
+          render :edit
+        end
+    end
+    
     def unsubscribe
         @customer = Customer.find(params[:id])
+    end
+    
+    def customer_params
+        params.require(:customer).permit(:first_name, :profile_image, :introduction)
     end
 end
