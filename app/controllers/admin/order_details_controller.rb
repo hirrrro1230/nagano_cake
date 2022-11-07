23e4c153add73ed4173f372c3c892ebdfde15f5a
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-11-07 12:49:09 +0000

    edit_making_status1107

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,4 +2,17 @@ class Admin::OrderDetailsController < ApplicationController
     def show
         
     end
+    
+    def update
+        @order_detail = OrderDetail.find(params[:id])
+        if @order_detail.update(order_detail_params)
+            redirect_to admin_order_path(@order_detail.order.id)
+        else
+            render :new
+        end
+    end
+    
+    def order_detail_params
+      params.require(:order_detail).permit(:making_status)
+    end 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,10 @@ class Admin::OrdersController < ApplicationController
     def show
         @order = Order.find(params[:id])
         #@customer = Customer.find(params[:id])
+        @orders = @order.order_details
+        #@order_detail = @order.order_detail
+        @order.shipping_cost = 800
+        @total = @order.total_payment + @order.shipping_cost
     end
     
     def update
@@ -17,5 +21,7 @@ class Admin::OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:status)
     end  
+    
+     
      
 end
